da36b8930f2dab7e4131872c8b6e764f6e62cd39
helosantosdesousa <helosantosdesousa@gmail.com>
2025-03-14 10:33:46 -0300

    first commit

---
+++ MS-Pagamento/src/main/java/com/github/helosantosdesousa/MS_Pagamento/MsPagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.github.helosantosdesousa.MS_Pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPagamentoApplication.class, args);
+	}
+
+}
